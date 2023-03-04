317ed6096fd79e3eaa826ce28e40d596306e19a5
Yisu Kim <yisu.kim.dev@outlook.com>
2023-03-04 09:35:41 +0000

    code: Write a checkbox functionality code

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -3,17 +3,19 @@ import { useState } from 'react';
 function App() {
   const [buttonColor, setButtonColor] = useState('red');
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+  const [disabled, setDisabled] = useState(false);
 
   return (
     <div>
       <button
         style={{ backgroundColor: buttonColor }}
         onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
       >
         Change to {newButtonColor}
       </button>
       <br />
-      <input type="checkbox" id="disable-button-checkbox" />
+      <input type="checkbox" id="disable-button-checkbox" onChange={(e) => setDisabled(e.target.checked)} />
     </div>
   );
 }
