d6eeb408bc9f7af3e5341e2b936789628eb2b2ed
Yisu Kim <yisu.kim.dev@outlook.com>
2023-03-04 09:04:22 +0000

    quiz: Move checkbox functionality quiz code

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [buttonColor, setButtonColor] = useState('red');
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        style={{ backgroundColor: buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+      >
+        Change to {newButtonColor}
+      </button>
+      <br />
+      <input type="checkbox" id="disable-button-checkbox" />
     </div>
   );
 }
