dbb13353346412c6dc48b01441a79f2a93e141d4
Yisu Kim <yisu.kim.dev@outlook.com>
2023-03-04 14:57:56 +0000

    quiz: Move colors with spaces quiz code

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
+export function replaceCamelWithSpaces(colorName) {
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
+}
+
 function App() {
   const [buttonColor, setButtonColor] = useState("red");
   const [disabled, setDisabled] = useState(false);
