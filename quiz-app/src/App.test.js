fe4c30620b4ce38193d787c2117262705ddaca2d
Yisu Kim <yisu.kim.dev@outlook.com>
2023-03-04 09:26:01 +0000

    test: Write a checkbox functionality test

--- quiz-app/src/App.test.js
+++ quiz-app/src/App.test.js
@@ -31,3 +31,31 @@ test("initial conditions", () => {
   const checkbox = screen.getByRole("checkbox");
   expect(checkbox).not.toBeChecked();
 });
+
+test("when the checkbox is checked, the button's active state should be toggled.", () => {
+  render(<App/>);
+
+  // find an element with a role of button
+  const colorButton = screen.getByRole("button", { name: "Change to blue"});
+
+  // expect the button is enabled
+  expect(colorButton).toBeEnabled();
+
+  // find an element with a role of checkbox
+  const checkbox = screen.getByRole("checkbox");
+
+  // click checkbox
+  fireEvent.click(checkbox);
+
+  // expect the checkbox is checked
+  expect(checkbox).toBeChecked();
+
+  // expect the button is disabled
+  expect(colorButton).toBeDisabled();
+
+  // click checkbox
+  fireEvent.click(checkbox);
+
+  // expect the button is enabled
+  expect(colorButton).toBeEnabled();
+})
