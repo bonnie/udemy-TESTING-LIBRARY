9fba4ab7e0dc86b2e5e82cca0d7b03f5b69177d8
Yisu Kim <yisu.kim.dev@outlook.com>
2023-03-04 15:06:42 +0000

    code: Write a colors with spaces code

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -5,10 +5,10 @@ export function replaceCamelWithSpaces(colorName) {
 }
 
 function App() {
-  const [buttonColor, setButtonColor] = useState("red");
+  const [buttonColor, setButtonColor] = useState("MediumVioletRed");
   const [disabled, setDisabled] = useState(false);
 
-  const newButtonColor = buttonColor === "red" ? "blue" : "red";
+  const newButtonColor = buttonColor === "MediumVioletRed" ? "MidnightBlue" : "MediumVioletRed";
 
   return (
     <div>
@@ -17,7 +17,7 @@ function App() {
         onClick={() => setButtonColor(newButtonColor)}
         disabled={disabled}
       >
-        Change to {newButtonColor}
+        Change to {replaceCamelWithSpaces(newButtonColor)}
       </button>
       <br />
       <input
