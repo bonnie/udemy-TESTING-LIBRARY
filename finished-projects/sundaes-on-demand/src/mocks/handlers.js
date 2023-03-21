e887c30b42faf3f28c05a382994ff5681beb13c1
Bonnie Schulkin <biz@bonnieschulkin.com>
2023-03-20 19:32:24 -0700

    Add 'sleep' function for loading test

--- finished-projects/sundaes-on-demand/src/mocks/handlers.js
+++ finished-projects/sundaes-on-demand/src/mocks/handlers.js
@@ -1,7 +1,12 @@
 import { rest } from "msw";
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export const handlers = [
-  rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
+  rest.get("http://localhost:3030/scoops", async (req, res, ctx) => {
+    await sleep(800);
     return res(
       ctx.json([
         { name: "Chocolate", imagePath: "/images/chocolate.png" },
@@ -18,7 +23,11 @@ export const handlers = [
       ])
     );
   }),
-  rest.post("http://localhost:3030/order", (req, res, ctx) => {
+  rest.post("http://localhost:3030/order", async (req, res, ctx) => {
+    // add a 100ms pause here to give jest a chance to see the "loading" state.
+    // See https://www.udemy.com/course/react-testing-library/learn/lecture/36703860
+    //   for more details.
+    await sleep(100);
     return res(ctx.json({ orderNumber: 123455676 }));
   }),
 ];
