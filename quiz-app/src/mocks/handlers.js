8118e49620cdceaf990eeeae16274a3558c2d53e
Yisu Kim <yisu.kim.dev@outlook.com>
2023-04-29 06:57:37 +0000

    quiz: Move topping options from server quiz code

---
+++ quiz-app/src/mocks/handlers.js
@@ -0,0 +1,12 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: "Chocolate", imagePath: "/images/chocolate.png" },
+        { name: "Vanilla", imagePath: "/images/vanilla.png" },
+      ])
+    );
+  }),
+];
---
+++ quiz-app/src/pages/entry/Options.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Row from "react-bootstrap/Row";
+import ScoopOption from "./ScoopOption";
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+
+  // optionType is 'scoops' or 'toppings
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        // TODO: handle error response
+      });
+  }, [optionType]);
+
+  // TODO: replace `null` with ToppingOption when available
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+}
