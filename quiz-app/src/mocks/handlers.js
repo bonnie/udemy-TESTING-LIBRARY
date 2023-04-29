65542fea6f182beb28a846b5c64915313862fb74
Yisu Kim <yisu.kim.dev@outlook.com>
2023-04-29 07:42:35 +0000

    test: Write some tests for the topping options from server

--- quiz-app/src/mocks/handlers.js
+++ quiz-app/src/mocks/handlers.js
@@ -9,4 +9,13 @@ export const handlers = [
       ])
     );
   }),
+  rest.get("http://localhost:3030/toppings", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: "Cherries", imagePath: "/images/cherries.png" },
+        { name: "M&Ms", imagePath: "/images/m-and-ms.png" },
+        { name: "Hot fudge", imagePath: "/images/hot-fudge.png" },
+      ])
+    );
+  }),
 ];
--- quiz-app/src/pages/entry/tests/Options.test.jsx
+++ quiz-app/src/pages/entry/tests/Options.test.jsx
@@ -9,7 +9,24 @@ test("displays image for each scoop option from server", async () => {
   expect(scoopImages).toHaveLength(2);
 
   // confirm alt text of images
-  // @ts-ignore
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("displays image for each topping option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  // find images
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  // confirm alt text of images
+  const altText = toppingImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
 });
