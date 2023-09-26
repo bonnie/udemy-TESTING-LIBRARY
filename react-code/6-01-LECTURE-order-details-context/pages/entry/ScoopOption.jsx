a00a42f06a1f8fbda1dad249eff3298e98f8dd6a
Bonnie Schulkin <biz@bonnieschulkin.com>
2023-09-26 09:14:50 -0700

    Add parseInt for scoop count value

--- react-code/6-01-LECTURE-order-details-context/pages/entry/ScoopOption.jsx
+++ react-code/6-01-LECTURE-order-details-context/pages/entry/ScoopOption.jsx
@@ -5,7 +5,8 @@ import { useOrderDetails } from "../../contexts/OrderDetails";
 
 export default function ScoopOptions({ name, imagePath }) {
   const { updateItemCount } = useOrderDetails();
-  const handleChange = (e) => updateItemCount(name, e.target.value, "scoops");
+  const handleChange = (e) =>
+    updateItemCount(name, parseInt(e.target.value), "scoops");
 
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
