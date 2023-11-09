e7d019b173ff573ab38adf0c247dc533a6cfbfac
Bonnie Schulkin <biz@bonnieschulkin.com>
2023-11-09 12:06:51 -0800

    Add react code for section 5

---
+++ react-code/5-02-QUIZ-topping-options-from-server/src/pages/entry/ToppingOption.jsx
@@ -0,0 +1,13 @@
+import Col from "react-bootstrap/Col";
+
+export default function ToppingOption({ name, imagePath }) {
+  return (
+    <Col xs={6} sm={4} md={3} lg={2} style={{ textAlign: "center" }}>
+      <img
+        style={{ width: "75%" }}
+        src={`http://localhost:3030/${imagePath}`}
+        alt={`${name} topping`}
+      />
+    </Col>
+  );
+}
