3a883642c6084a1f310136c474cc270b2ee6df5f
Bonnie Schulkin <biz@bonnieschulkin.com>
2023-11-12 08:17:03 -0800

    Add App.test.jsx file

---
+++ vite-starter/src/App.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("App contains correct heading", () => {
+  render(<App />);
+  const headingElement = screen.getByText(/learn react/i);
+  expect(headingElement).toBeInTheDocument();
+});
