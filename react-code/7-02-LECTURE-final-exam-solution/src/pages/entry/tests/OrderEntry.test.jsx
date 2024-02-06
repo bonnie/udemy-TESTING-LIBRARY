425838bf8d000a212377fd88f9b1128c78d248b4
Bonnie Schulkin <biz@bonnieschulkin.com>
2024-02-05 19:17:25 -0800

    Add final exam solution code

---
+++ react-code/7-02-LECTURE-final-exam-solution/src/pages/entry/tests/OrderEntry.test.jsx
@@ -0,0 +1,20 @@
+import { render, screen } from "../../../test-utils/testing-library-utils";
+import { HttpResponse, http } from "msw";
+import { server } from "../../../mocks/server";
+import OrderEntry from "../OrderEntry";
+
+test("handles error for scoops and toppings routes", async () => {
+  server.resetHandlers(
+    http.get("http://localhost:3030/scoops", () => {
+      return new HttpResponse(null, { status: 500 });
+    }),
+    http.get("http://localhost:3030/toppings", () => {
+      return new HttpResponse(null, { status: 500 });
+    })
+  );
+
+  render(<OrderEntry />);
+
+  const alerts = await screen.findAllByRole("alert");
+  expect(alerts).toHaveLength(2);
+});
---
+++ react-code/7-02-LECTURE-final-exam-solution/src/test-utils/testing-library-utils.jsx
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+import { OrderDetailsProvider } from "../contexts/OrderDetails";
+
+const renderWithContext = (ui, options) =>
+  render(ui, { wrapper: OrderDetailsProvider, ...options });
+
+// re-export everything
+export * from "@testing-library/react";
+
+// override render method
+export { renderWithContext as render };
