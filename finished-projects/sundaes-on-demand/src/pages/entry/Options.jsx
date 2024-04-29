f70bf22044b5216de7f0b1940d90180a74c412cd
Bonnie Schulkin <biz@bonnieschulkin.com>
2024-04-28 18:25:06 -0700

    Add useEffect cleanup

--- finished-projects/sundaes-on-demand/src/pages/entry/Options.jsx
+++ finished-projects/sundaes-on-demand/src/pages/entry/Options.jsx
@@ -15,10 +15,15 @@ export default function Options({ optionType }) {
 
   // optionType is 'scoops' or 'toppings
   useEffect(() => {
+    const controller = new AbortController();
     axios
-      .get(`http://localhost:3030/${optionType}`)
+      .get(`http://localhost:3030/${optionType}`, { signal: controller.signal })
       .then((response) => setItems(response.data))
-      .catch((error) => setError(true));
+      .catch((error) => {
+        if (error.name !== "CanceledError") setError(true);
+      });
+
+    return () => controller.abort();
   }, [optionType]);
 
   if (error) {
