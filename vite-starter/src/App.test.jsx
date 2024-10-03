c8ad66385268c634cea4d5e73aae267d8920e9b4
Oleh <88395278+Oleh-Li@users.noreply.github.com>
2024-10-03 22:02:16 +0300

    init commit

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("App contains correct heading", () => {
-  render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
+  render(<App />); //simulated DOM
+  const headingElement = screen.getByText(/learn react/i); //find element be display text
   expect(headingElement).toBeInTheDocument();
 });
